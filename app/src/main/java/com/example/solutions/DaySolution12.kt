dbf9d6f835d43725c2af09240dfa1a8d3775b559
Tamara Sineva <sinjvf@gmail.com>
2025-12-12 11:56:06 +0300

    day12

--- app/src/main/java/com/example/solutions/DaySolution12.kt
+++ app/src/main/java/com/example/solutions/DaySolution12.kt
@@ -1,19 +1,72 @@
 package com.example.solutions
 
 import com.example.ILogger
+import com.example.aoc25.util.Matrix
 
 class DaySolution12(private val logger: ILogger) : DaySolution {
+    class Tree(val x: Int, val y: Int, val data: List<Int>, val str:String){
+        override fun toString(): String {
+            return str
+        }
+    }
 
     override val part1 = object : DaySolutionPart {
         private var intRes: Long = 0
+        private var flag = false
+        private val forms = mutableMapOf<Int, Matrix<Char>>()
+        private val spaces = mutableMapOf<Int, Int>()
 
+        private val trees = mutableListOf<Tree>()
+        private var currForm = 0
 
         override fun handleLine(inputStr: String, pos: Int) {
 
+            if (inputStr.isEmpty()) return
+            if (flag || inputStr.contains("x")) {
+                flag = true
+                val s1 = inputStr.split(":")
+                val s2 = s1[0].split("x").map { it.toInt() }
+                trees.add(
+                    Tree(
+                        s2[0],
+                        s2[1],
+                        s1[1].split(" ").filter { it.isNotBlank() }.map { it.toInt() }, inputStr)
+                )
+            } else {
+                if (inputStr.contains(":")) {
+                    currForm = inputStr[0].toString().toInt()
+                    forms[currForm] = Matrix()
+                } else {
+                    val m = forms[currForm]!!
+                    forms[currForm]!!.addRaw(m.ySize, inputStr.toList())
+                }
+            }
         }
 
         override fun finish() {
 
+            forms.forEach {
+                val m = it.value
+                val iterator = m.iterator()
+                var size = 0
+                while (iterator.hasNext()) {
+                    val next = iterator.next()
+                    if (next.data == '#') size++
+                }
+                spaces.put(it.key, size)
+            }
+
+            intRes = trees.count { enought(it) }.toLong()
+        }
+
+        private fun enought(tree: Tree):Boolean {
+            val treeSize = tree.y * tree.x
+            var giftSize = 0
+             tree.data.forEachIndexed { index, i ->
+                giftSize += spaces[index]!! * i
+            }
+            logger.logD("tree = $tree \n $treeSize, $giftSize")
+            return treeSize > giftSize
         }
 
         override fun obtainResult(): String = intRes.toString()
--- app/src/test/java/com/example/aoc25/DaySolution12Test.kt
+++ app/src/test/java/com/example/aoc25/DaySolution12Test.kt
@@ -45,7 +45,39 @@ class DaySolution12Test {
     }
 
     private val test_input12_1 =
-        """""".trimIndent().split("\n")
+        """0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2""".trimIndent().split("\n")
     private val result1 = "11"
 
     private val test_input12_2 = test_input12_1
